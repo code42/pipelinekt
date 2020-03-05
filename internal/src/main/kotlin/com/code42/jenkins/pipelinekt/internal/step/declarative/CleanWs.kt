ad5201f6edcb71aeba7d7bea156b1b48b614e66d
Erik Rasmussen <erik.rasmussen@code42.com>
2020-03-05 11:01:55 -0700

    Add default parameters to cleanWs/CleanWs for deleteDirs and disableDeferredWipeout (false) to make it backward compatible, but allow for setting them.

--- internal/src/main/kotlin/com/code42/jenkins/pipelinekt/internal/step/declarative/CleanWs.kt
+++ internal/src/main/kotlin/com/code42/jenkins/pipelinekt/internal/step/declarative/CleanWs.kt
@@ -4,8 +4,17 @@ import com.code42.jenkins.pipelinekt.core.step.DeclarativeStep
 import com.code42.jenkins.pipelinekt.core.step.SingletonStep
 import com.code42.jenkins.pipelinekt.core.writer.GroovyWriter
 
-object CleanWs : DeclarativeStep, SingletonStep {
+data class CleanWs(val deleteDirs: Boolean, val disableDeferredWipeout: Boolean) : DeclarativeStep, SingletonStep {
     override fun toGroovy(writer: GroovyWriter) {
-        writer.writeln("cleanWs()")
+        writer.writeln("cleanWs(")
+        writer.inner().let { inner ->
+            if (deleteDirs && disableDeferredWipeout)
+                inner.writeln("deleteDirs: true, disableDeferredWipeout: true")
+            else if (deleteDirs)
+                inner.writeln("deleteDirs: true")
+            else if (disableDeferredWipeout)
+                inner.writeln("disableDeferredWipeout: true")
+        }
+        writer.writeln(")")
     }
 }
