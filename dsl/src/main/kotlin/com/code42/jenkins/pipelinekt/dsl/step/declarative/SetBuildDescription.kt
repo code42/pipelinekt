c80807f96adc970716bc5457f76dfdbd36fe99cd
Caleb Reder <rede0060@d.umn.edu>
2021-10-08 10:19:11 -0500

    Adding dsl classes for getting at the ability to set build descriptions (#29)
    Co-authored-by: Caleb Reder <caleb.reder@code42.com>

---
+++ dsl/src/main/kotlin/com/code42/jenkins/pipelinekt/dsl/step/declarative/SetBuildDescription.kt
@@ -0,0 +1,8 @@
+package com.code42.jenkins.pipelinekt.dsl.step.declarative
+
+import com.code42.jenkins.pipelinekt.core.step.Step
+import com.code42.jenkins.pipelinekt.dsl.DslContext
+import com.code42.jenkins.pipelinekt.internal.step.scripted.SetBuildDescription
+
+fun DslContext<Step>.setBuildDescription(description: String) =
+        add(SetBuildDescription(description))
