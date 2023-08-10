1f3d81fc44bace00074ced39d1088967a11a072f
Arthur Ramsey <arthur.ramsey@code42.com>
2023-08-10 10:03:09 -0500

    Support allow empty junit results

--- dsl/src/main/kotlin/com/code42/jenkins/pipelinekt/dsl/step/declarative/JUnitDsl.kt
+++ dsl/src/main/kotlin/com/code42/jenkins/pipelinekt/dsl/step/declarative/JUnitDsl.kt
@@ -7,7 +7,12 @@ import com.code42.jenkins.pipelinekt.dsl.DslContext
 import com.code42.jenkins.pipelinekt.internal.step.declarative.JUnit
 
 fun DslContext<Step>.junit(testResults: String) =
+        junit(testResults = testResults.strDouble(), false)
+
+fun DslContext<Step>.junit(testResults: String, allowEmptyResults: Boolean) =
         junit(testResults = testResults.strDouble())
 
 fun DslContext<Step>.junit(testResults: Var.Literal.Str) =
-        add(JUnit(testResults = testResults))
+        add(JUnit(testResults = testResults. false))
+fun DslContext<Step>.junit(testResults: Var.Literal.Str, allowEmptyResults: Boolean) =
+        add(JUnit(testResults = testResults. allowEmptyResults=allowEmptyResults))
