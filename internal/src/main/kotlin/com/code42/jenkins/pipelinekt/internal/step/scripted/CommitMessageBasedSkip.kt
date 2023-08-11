d7bf654a9cf98141e8e3f6b6925c0dab61952583
Arthur Ramsey <54082931+c42-arthur@users.noreply.github.com>
2023-08-11 13:11:07 -0500

    Support commit message based skip (#43)

---
+++ internal/src/main/kotlin/com/code42/jenkins/pipelinekt/internal/step/scripted/CommitMessageBasedSkip.kt
@@ -0,0 +1,32 @@
+package com.code42.jenkins.pipelinekt.internal.step.scripted
+
+import com.code42.jenkins.pipelinekt.core.step.ScriptedStep
+import com.code42.jenkins.pipelinekt.core.step.SingletonStep
+import com.code42.jenkins.pipelinekt.core.writer.GroovyWriter
+
+/**
+ * Use this to abort a job based on a regex pattern match in the latest commit message.
+ * 
+ * This requires approval of the following signature which isn't normally allowed in the Groovy sandbox.
+ * new org.jenkinsci.plugins.workflow.steps.FlowInterruptedException hudson.model.Result jenkins.model.CauseOfInterruption[]
+ */
+
+@Suppress("MaxLineLength")
+data class CommitMessageBasedSkip(
+    val regex: String = "\\[ci skip\\]",
+    val result: String = "SUCCESS"
+) : ScriptedStep, SingletonStep {
+    override fun scriptedGroovy(writer: GroovyWriter) {
+        writer.writeln(
+            """
+                script {
+                    try {
+                        sh "git log --oneline -1 | grep -qE '$regex' && false"
+                    } catch(exc) {
+                        throw new FlowInterruptedException(Result.$result)
+                    }
+                }
+            """.trimIndent()
+        )
+    }
+}
