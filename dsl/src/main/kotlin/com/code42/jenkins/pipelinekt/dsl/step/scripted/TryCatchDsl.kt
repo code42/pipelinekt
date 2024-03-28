621d829695fafc56ff122b3c42a56c894b7d6d0a
forest-code42 <130488542+forest-code42@users.noreply.github.com>
2024-03-28 14:13:44 -0500

    Oops, fix parameter naming (#55)

--- dsl/src/main/kotlin/com/code42/jenkins/pipelinekt/dsl/step/scripted/TryCatchDsl.kt
+++ dsl/src/main/kotlin/com/code42/jenkins/pipelinekt/dsl/step/scripted/TryCatchDsl.kt
@@ -7,22 +7,21 @@ import com.code42.jenkins.pipelinekt.internal.step.scripted.Try
 
 @Suppress("UnusedPrivateMember")
 fun DslContext<Step>.`try`(
-        trySteps: DslContext<Step>.() -> Unit,
-        `catch`: (DslContext<Step>.() -> Unit)?
+    trySteps: DslContext<Step>.() -> Unit,
+    catchSteps: (DslContext<Step>.() -> Unit)?
 ) {
-    tryCatch(trySteps, `catch`)
+    tryCatch(trySteps, catchSteps)
 }
 
 fun DslContext<Step>.tryCatch(
-        tryStep: DslContext<Step>.() -> Unit,
-        catchStep: (DslContext<Step>.() -> Unit)?
+    tryStep: DslContext<Step>.() -> Unit,
+    catchStep: (DslContext<Step>.() -> Unit)?
 ) {
     val trySteps = DslContext.into(tryStep).toStep()
-    if(catchStep != null) {
+    if (catchStep != null) {
         val catchSteps = DslContext.into(catchStep).toStep()
         add(Try(trySteps, catchSteps))
     } else {
         add(Try(trySteps, null))
     }
 }
-
